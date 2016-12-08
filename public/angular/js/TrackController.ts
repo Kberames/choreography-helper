a0717c79b0fd88dcc8dba40effad36e50e7eccc3
Kimberly Ames <ameskimberly@gmail.com>
2016-12-07 17:16:02 -0700

    Added POST for track.

--- public/angular/js/TrackController.ts
+++ public/angular/js/TrackController.ts
@@ -14,28 +14,46 @@ namespace App {
             this.httpService = $http;
             this.stateService = $state;
 
-            console.log ('Passed parameters: ', this.stateService.params);
-            this.httpService ({
-              url: '/media/track/' + this.stateService.params.id,
-              method: 'GET'
-            })
-            .success ((response) => {
-              console.log (response);
-              this.track = response;
-            })
-            .error (() => {
-            })
+            if (this.stateService.params) {
+                console.log ('Passed parameters: ', this.stateService.params);
+
+                this.httpService ({
+                  url: '/media/track/' + this.stateService.params.id,
+                  method: 'GET'
+                })
+                .success ((response) => {
+                  console.log (response);
+                  this.track = response;
+                })
+                .error (() => {
+                })
+            }
+            else {
+                console.log ('creating new track - inside track constructor');
+            }
         }
 
         public saveTrack () {
             console.log ('track save method fired for track: ' + this.stateService.params.id);
 
+            let method = '';
+            let url = '';
+
             let updateID = this.stateService.params.id;
+
+            if (updateID == -1) {
+                method = 'POST';
+                url = '/media/track';
+            }
+            else {
+                method = 'PUT';
+                url = '/media/track/' + updateID;
+            }
 
             this.httpService ({
                 // Need to include media the path.
-                url: '/media/track/' + updateID,
-                method: 'PUT',
+                url: url,
+                method: method,
                 data: this.track
             })
             .success ((response) => {
--- routes/media.js
+++ routes/media.js
@@ -12,41 +12,98 @@ var Presenter = require ('../model/presenter.js');
 
 // Define routes.
 
-// Route to view my releases.
-router.get ('/', function (request, response) {
-    // response.send ('This is where we list releases.');
+//******************************
+// RELEASE routes.
+//******************************
+    // Route to view my releases.
+    router.get ('/', function (request, response) {
+        // response.send ('This is where we list releases.');
 
-    Release.find ({}, function (error, result){
-        if (error) {
-            var errorMessage = 'Unable to load releases.';
-            console.error ('*** ERROR: ' + errorMessage);
-            response.send (errorMessage);
-        }
-        else {
-            console.log('ReleaseList find: ' + result);
-            response.json (result);
-        }
-    });
-});
-
-// Route to grab a specific release by it's id.
-router.get ('/release/:id', function (request, response) {
-    // Grab the release id by the ':id' value in the url path.
-    var releaseId = request.params.id;
-    console.log('inside get release by id');
-
-    // Use the mongoose query builder to grab the
-    // release.
-    Release.findById(releaseId)
-    .populate({
-        path: 'tracks',
-    })
-    .populate({
-        path: 'presenters',
-    })
-        .exec (function (error, result) {
+        Release.find ({}, function (error, result){
             if (error) {
-                var errorMessage = 'Unable to load release.';
+                var errorMessage = 'Unable to load releases.';
+                console.error ('*** ERROR: ' + errorMessage);
+                response.send (errorMessage);
+            }
+            else {
+                console.log('ReleaseList find: ' + result);
+                response.json (result);
+            }
+        });
+    });
+
+    // Route to grab a specific release by it's id.
+    router.get ('/release/:id', function (request, response) {
+        // Grab the release id by the ':id' value in the url path.
+        var releaseId = request.params.id;
+        console.log('inside get release by id');
+
+        // Use the mongoose query builder to grab the
+        // release.
+        Release.findById(releaseId)
+        .populate({
+            path: 'tracks',
+        })
+        .populate({
+            path: 'presenters',
+        })
+            .exec (function (error, result) {
+                if (error) {
+                    var errorMessage = 'Unable to load release.';
+                    console.error ('*** ERROR: ' + errorMessage);
+                    response.send (errorMessage);
+                }
+                else {
+                    console.log('Release findOne: ' + result);
+                    response.json (result);
+                }
+            });
+    });
+
+    // Create a route to handle updating an existing release.
+    router.put ('/release/:id', function (request, response) {
+        var releaseId = request.params.id;
+
+        console.log('releaseId to update: ' + request.params.id);
+
+        console.log('request body: ', request.body);
+
+        Release.findByIdAndUpdate (
+            // id to search by
+            releaseId,
+
+            // What needs to be udpdated.
+            request.body,
+
+            // Callback function.
+            function (error, result) {
+                if (error) {
+                    var errorMessage = 'Unable to save release.';
+                    console.error ('*** ERROR: ' + errorMessage);
+                    response.send (errorMessage);
+                }
+                else {
+                        response.json ({
+                            message: 'Release was updated.'});
+                }
+            }
+        );
+    });
+
+//******************************
+// PRESENTER routes.
+//******************************
+    // Route to grab a specific presenter by their id.
+    router.get ('/presenter/:id', function (request, response) {
+        // Grab the release id by the ':id' value in the url path.
+        var presenterId = request.params.id;
+        console.log('inside get presenter by id');
+
+        // Use the mongoose query builder to grab the
+        // release.
+        Presenter.findById(presenterId, function (error, result) {
+            if (error) {
+                var errorMessage = 'Unable to load presenter.';
                 console.error ('*** ERROR: ' + errorMessage);
                 response.send (errorMessage);
             }
@@ -55,139 +112,143 @@ router.get ('/release/:id', function (request, response) {
                 response.json (result);
             }
         });
-});
+    });
 
-// Create a route to handle updating an existing release.
-router.put ('/release/:id', function (request, response) {
-    var releaseId = request.params.id;
+    // Create a route to handle updating an existing presenter.
+    router.put ('/presenter/:id', function (request, response) {
+        var presenterId = request.params.id;
 
-    console.log('releaseId to update: ' + request.params.id);
+        console.log('presenterId to update: ' + request.params.id);
 
-    console.log('request body: ', request.body);
+        console.log('request body: ', request.body);
 
-    Release.findByIdAndUpdate (
-        // id to search by
-        releaseId,
+        Presenter.findByIdAndUpdate (
+            // id to search by
+            presenterId,
 
-        // What needs to be udpdated.
-        request.body,
+            // What needs to be udpdated.
+            request.body,
 
-        // Callback function.
-        function (error, result) {
+            // Callback function.
+            function (error, result) {
+                if (error) {
+                    var errorMessage = 'Unable to save presenter.';
+                    console.error ('*** ERROR: ' + errorMessage);
+                    response.send (errorMessage);
+                }
+                else {
+                        response.json ({
+                            message: 'Presenter was updated.'});
+                }
+            }
+        );
+    });
+
+//******************************
+// TRACK routes.
+//******************************
+    // Route to save a new track.
+    router.post ('/track', function (request, response) {
+        console.log('inside track post');
+
+        // Create a new track from the data sent
+        // down by form or API post.
+        var newTrack = Track (request.body);
+
+        console.log ("body: ", request.body);
+        console.log ("body: ", newTrack);
+        newTrack.save (function (error) {
+            //Check for errors.
             if (error) {
-                var errorMessage = 'Unable to save release.';
+                var errorMessage = 'Unable to save the track.';
                 console.error ('*** ERROR: ' + errorMessage);
                 response.send (errorMessage);
             }
             else {
-                    response.json ({
-                        message: 'Release was updated.'});
+                response.json ({
+                    message: 'New track was saved.'
+                });
             }
-        }
-    );
-});
+        });
+    });
 
-// Route to grab a specific presenter by their id.
-router.get ('/presenter/:id', function (request, response) {
-    // Grab the release id by the ':id' value in the url path.
-    var presenterId = request.params.id;
-    console.log('inside get presenter by id');
+    // Route to grab a specific track by their id.
+    router.get ('/track/:id', function (request, response) {
+        // Grab the release id by the ':id' value in the url path.
+        var trackId = request.params.id;
+        console.log('inside get track by id');
 
-    // Use the mongoose query builder to grab the
-    // release.
-    Presenter.findById(presenterId, function (error, result) {
-        if (error) {
-            var errorMessage = 'Unable to load presenter.';
-            console.error ('*** ERROR: ' + errorMessage);
-            response.send (errorMessage);
+        if (trackId == -1) {
+            console.log('NEW BLANK Track');
+            response.json ({});
         }
         else {
-            console.log('Release findOne: ' + result);
-            response.json (result);
+            // Use the mongoose query builder to grab the
+            // release.
+            Track.findById(trackId, function (error, result) {
+                if (error) {
+                    var errorMessage = 'Unable to load track.';
+                    console.error ('*** ERROR: ' + errorMessage);
+                    response.send (errorMessage);
+                }
+                else {
+                    console.log('Track findOne: ' + result);
+                    response.json (result);
+                }
+            });
         }
     });
-});
 
-// Create a route to handle updating an existing presenter.
-router.put ('/presenter/:id', function (request, response) {
-    var presenterId = request.params.id;
+    // Create a route to handle updating an existing track.
+    router.put ('/track/:id', function (request, response) {
+        var trackId = request.params.id;
 
-    console.log('presenterId to update: ' + request.params.id);
+        console.log('trackId to update: ' + request.params.id);
 
-    console.log('request body: ', request.body);
+        console.log('request body: ', request.body);
 
-    Presenter.findByIdAndUpdate (
-        // id to search by
-        presenterId,
+        Track.findByIdAndUpdate (
+            // id to search by
+            trackId,
 
-        // What needs to be udpdated.
-        request.body,
+            // What needs to be udpdated.
+            request.body,
 
-        // Callback function.
-        function (error, result) {
+            // Callback function.
+            function (error, result) {
+                if (error) {
+                    var errorMessage = 'Unable to save track.';
+                    console.error ('*** ERROR: ' + errorMessage);
+                    response.send (errorMessage);
+                }
+                else {
+                        response.json ({
+                            message: 'Track was updated.'});
+                }
+            }
+        );
+    });
+
+    // Create a route to handle creating an existing track.
+    router.post ('/track', function (request, response) {
+        var newTrack = Track (request.body);
+
+        console.log ("body: ", request.body);
+        console.log ("body: ", newTrack);
+        newTrack.save (function (error) {
+            //Check for errors.
             if (error) {
-                var errorMessage = 'Unable to save presenter.';
+                var errorMessage = 'Unable to save the track.';
                 console.error ('*** ERROR: ' + errorMessage);
                 response.send (errorMessage);
             }
             else {
-                    response.json ({
-                        message: 'Presenter was updated.'});
+                response.json ({
+                    message: 'New track was saved.'
+                });
             }
-        }
-    );
-});
-
-// Route to grab a specific track by their id.
-router.get ('/track/:id', function (request, response) {
-    // Grab the release id by the ':id' value in the url path.
-    var trackId = request.params.id;
-    console.log('inside get track by id');
-
-    // Use the mongoose query builder to grab the
-    // release.
-    Track.findById(trackId, function (error, result) {
-        if (error) {
-            var errorMessage = 'Unable to load track.';
-            console.error ('*** ERROR: ' + errorMessage);
-            response.send (errorMessage);
-        }
-        else {
-            console.log('Track findOne: ' + result);
-            response.json (result);
-        }
+        });
     });
-});
-
-// Create a route to handle updating an existing track.
-router.put ('/track/:id', function (request, response) {
-    var trackId = request.params.id;
-
-    console.log('trackId to update: ' + request.params.id);
-
-    console.log('request body: ', request.body);
-
-    Track.findByIdAndUpdate (
-        // id to search by
-        trackId,
-
-        // What needs to be udpdated.
-        request.body,
-
-        // Callback function.
-        function (error, result) {
-            if (error) {
-                var errorMessage = 'Unable to save track.';
-                console.error ('*** ERROR: ' + errorMessage);
-                response.send (errorMessage);
-            }
-            else {
-                    response.json ({
-                        message: 'Track was updated.'});
-            }
-        }
-    );
-});
 
 // Route to test.
 router.get ('/test', function (request, response) {
